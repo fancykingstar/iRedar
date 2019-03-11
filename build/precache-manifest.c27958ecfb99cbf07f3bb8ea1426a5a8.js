5b1945a18d937a1afd56c9955b35bfbc97a50096
Chigozie Beluolisa <cbeluolisa@hotmail.com>
2019-03-10 23:34:08 -0400

    add build

---
+++ build/precache-manifest.c27958ecfb99cbf07f3bb8ea1426a5a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "365c7d54a5277bd3ebc6",
+    "url": "/static/js/main.b4e6eb65.chunk.js"
+  },
+  {
+    "revision": "504945bb6aa55886c6d8",
+    "url": "/static/js/2.eb594a5c.chunk.js"
+  },
+  {
+    "revision": "365c7d54a5277bd3ebc6",
+    "url": "/static/css/main.7bb3c5d4.chunk.css"
+  },
+  {
+    "revision": "504945bb6aa55886c6d8",
+    "url": "/static/css/2.8b7b53a3.chunk.css"
+  },
+  {
+    "revision": "e59fc94e8c48a93a48ceee18b1c58ee2",
+    "url": "/index.html"
+  }
+];
