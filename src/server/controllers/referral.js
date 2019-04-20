e8af8850d86a206626fd13f9040fed6085aae4de
Chigozie Beluolisa <cbeluolisa@hotmail.com>
2019-04-20 19:34:48 -0400

    Removed unsed variable

--- src/server/controllers/referral.js
+++ src/server/controllers/referral.js
@@ -8,8 +8,6 @@ const User = require('../models/User')
 const Profile = require('../models/Profile')
 const Referral = require('../models/Referral')
 
-const { referralPreview } = require('../helpers/htmlMails/referral');
-
 exports.getAllReferrals = async (req, res) => {
     try {
         let userId = req.user._id
