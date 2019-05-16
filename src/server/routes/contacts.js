0d1f1edf2804a92c91db7db56e9072701c048180
Gideon Rosales <gideonrosales@gmail.com>
2019-05-16 17:23:02 +0800

    feature: add CRUD to contact module

---
+++ src/server/routes/contacts.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+const contactController = require('../controllers/contact');
+
+/**
+ * @route /api/contacts/
+ * @description Get the list of contacts
+ * @access Private
+ **/
+router.get('/', contactController.index);
+
+/**
+ * @route /api/contacts/store
+ * @description Store new contact resource
+ * @access Private
+ **/
+router.post('/store', contactController.store);
+
+/**
+ * @route /api/contacts/:id/edit
+ * @description Get the contact resource
+ * @access Private
+ **/
+router.get('/:id/edit', contactController.edit);
+
+/**
+ * @route /api/contacts/:id/update
+ * @description Update the contact resource
+ * @access Private
+ **/
+router.patch('/:id/update', contactController.update);
+
+/**
+ * @route /api/contacts/:id/delete
+ * @description Delete the contact resource
+ * @access Private
+ **/
+router.delete('/:id/delete', contactController.delete);
+
+module.exports = router;
