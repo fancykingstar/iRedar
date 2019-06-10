6df08e9846ece680bef602ebc3944b19756f7207
king-star <itsolution0820@gmail.com>
2019-06-10 07:26:10 -0400

    mlab change

--- src/server/configs/keys_dev.js
+++ src/server/configs/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //mongoURI: 'mongodb://iauto:iauto2019@ds161764.mlab.com:61764/iauto-api-dev',
-  mongoURI: 'mongodb://localhost:27017/iAutoData',
+  mongoURI: 'mongodb://iauto:iauto2019@ds161764.mlab.com:61764/iauto-api-dev',
+  // mongoURI: 'mongodb://localhost:27017/iAutoData',
   testDB: 'mongodb://test:test123@ds163053.mlab.com:63053/mock-test',
   secretOrKey: 'jjgfawertkhjkhgszfdfghhkluy',
   email: 'forms@iradardata.com',
